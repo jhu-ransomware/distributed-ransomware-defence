31d3912e42f8a4ecc7d0bee58bf4e7df580a00dc
damianomarsili <dmarsil1@jh.edu>
2022-10-05 15:42:29 -0400

    diagnose implementation

--- diagnose.c
+++ diagnose.c
@@ -1,1 +1,18 @@
 #include "diagnose.h"
+
+
+int * diagnose(int tested_up[], int index) {
+    int state_x[NUM_NODES];
+
+    for (int i = 0; i < NUM_NODES; i++) {
+        state_x[i] = 1;
+    }
+
+    int ptr = index;
+    while (ptr == index) {
+        state_x[ptr] = 0;
+        ptr = tested_up[ptr];
+    } 
+
+    return state_x;
+}
--- diagnose.h
+++ diagnose.h
@@ -1,4 +1,8 @@
 #ifndef DIAGNOSE_H
 #define DIAGNOSE_H
 
+#include "constants.h"
+
+int * diagnose(int tested_up[], int index);
+
 #endif
