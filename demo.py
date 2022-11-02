43b676e622970c1adb2ac8ec467dd1cb74841b93
Nicholas Bowen <nbowen3@jhu.edu>
2022-11-02 14:38:17 -0400

    added drawing to the screen

--- demo.py
+++ demo.py
@@ -1,39 +1,76 @@
-from re import I
 import socket
 import sys
+import pygame, sys
+from pygame.locals import *
 
-PORT = 10100
 NUM_NODES = 2
+results = [0 for element in range(10)]
 
-INT_SIZE_C = 4
+def draw_rect(display):
+    WHITE=(255,255,255)
+    display.fill(WHITE)
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    GREEN = (0,255,0)
+    RED = (255,0,0)
 
-server_address = ('', PORT)
-print(f"starting up on {server_address} port {PORT}")
-sock.bind(server_address)
-sock.listen(NUM_NODES)
+    GAP = 10
+    SIZE_RECT = 50
 
-while True:
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
+    for i in range(0, NUM_NODES):
+        color = RED if results[i] else GREEN
+        offset = (GAP + SIZE_RECT) * i
+        pygame.draw.rect(display,color,(offset + SIZE_RECT,(400 - SIZE_RECT) / 2,SIZE_RECT,SIZE_RECT))
 
-    try:
-        print(f"connection from {client_address}")
+def draw(sock):
+    pygame.init()
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(1024)
-            node = int.from_bytes(data[:4], byteorder=sys.byteorder)
-            diagnosis = []
-            for i in range(4, 4 + 4  * NUM_NODES, 4):
-                print("Here")
-                diagnosis.append(int.from_bytes(data[i:i+4], byteorder=sys.byteorder))
-            
-            print(f"Node {node} diagnosis:\n{diagnosis[4:]}")
-            break
+    DISPLAY=pygame.display.set_mode((500,400),0,32)
 
-    finally:
-        # Clean up the connection
-        connection.close()
+    draw_rect(DISPLAY)
+
+    while True:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
+
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
+
+        try:
+            print(f"connection from {client_address}")
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(1024)
+                node = int.from_bytes(data[:4], byteorder=sys.byteorder)
+                diagnosis = []
+                for i in range(4, 4 + 4  * NUM_NODES, 4):
+                    print("Here")
+                    diagnosis.append(int.from_bytes(data[i:i+4], byteorder=sys.byteorder))
+                
+                print(f"Node {node} diagnosis:\n{diagnosis[4:]}")
+                break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+        
+        draw_rect(DISPLAY)
+
+def main():
+    PORT = 10200
+
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('', PORT)
+    print(f"starting up on {server_address} port {PORT}")
+    sock.bind(server_address)
+    sock.listen(NUM_NODES)
+
+    draw(sock)
+
+if __name__ == "__main__":
+    main()
